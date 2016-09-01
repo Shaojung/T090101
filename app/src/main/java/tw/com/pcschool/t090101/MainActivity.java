671deefbe0210911e3d5bc9038bf6955b99f0c0d
Shaojung <shaojung@gmail.com>
2016-09-01 20:01:37 +0800

    Volley -> JSON -> ListView

--- app/src/main/java/tw/com/pcschool/t090101/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t090101/MainActivity.java
@@ -3,6 +3,8 @@ package tw.com.pcschool.t090101;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -15,12 +17,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv;
+    ArrayAdapter<String> adapter;
+    String ani[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        lv = (ListView) findViewById(R.id.listView);
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         final StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
                 new Response.Listener<String>() {
@@ -30,19 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
                         try {
                             JSONArray array = new JSONArray(response);
+                            ani = new String[array.length()];
                             for (int i=0;i<array.length();i++)
                             {
                                 JSONObject obj = array.getJSONObject(i);
                                 String str = obj.getString("district");
+                                ani[i] = str;
                                 Log.d("NET", str);
                             }
-
-
+                            adapter = new ArrayAdapter<String>(MainActivity.this,
+                                                    android.R.layout.simple_list_item_1,
+                                                    ani);
+                            lv.setAdapter(adapter);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
 
                     }
                 }, new Response.ErrorListener() {
